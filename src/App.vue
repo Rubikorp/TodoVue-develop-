1746d14fa5b6e58d27659843db01d638d882b8f2
Ruslan Bikmetov <bik.big102rus@gmail.com>
2024-01-24 13:33:10 +0500

    refactor and style

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <button class="button-focus">Hello</button>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    computed: {
+        ...mapGetters(["get_state", "get_state_find_words"]),
     }
-  }
 }
+
+
+</script>
+
+<style lang="scss">
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,39 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import { v4 as uuidv4 } from "uuid";
 
 export default createStore({
   state: {
+    todos: [
+      {
+        id: uuidv4(),
+        text: "Планируй и создавай",
+        flag: false,
+      },
+    ],
   },
   getters: {
+    get_state(state) {
+      return state.todos;
+    },
+
+    get_state_find_words(todos = state.todos, input) {
+      const matches = [];
+      todos.forEach((todo) => {
+        if (todo.text.indexOf(input) !== -1) {
+          matches.push(todo);
+        }
+      });
+      return matches;
+    },
   },
   mutations: {
+    add_todo(state, todo) {
+      state.todos.push(todo)
+    },
+    delete_todo(state, id) {
+      state.todos.filter((todo) => todo.id !== id)
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
